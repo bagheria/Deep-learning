214e4eb6b9cd0d7e3ef0d56914d26bb5452deaf5
Bagheri <a.bagheri@uu.nl>
2019-01-28 12:46:07 +0100

    Update mnist-regularization.py

--- regularization/mnist-regularization.py
+++ regularization/mnist-regularization.py
@@ -2,6 +2,9 @@ from keras import models
 from keras.layers import Dense, Dropout
 from keras.utils import to_categorical
 from keras.datasets import mnist
+from keras import regularizers
+import matplotlib.pyplot as plt
+from keras.callbacks import EarlyStopping
 
 
 def data_summary(X_train, y_train, X_test, y_test):
@@ -19,11 +22,34 @@ def data_summary(X_train, y_train, X_test, y_test):
 # Check state of dataset
 data_summary(X_train, y_train, X_test, y_test)
 
+# Create a grid of 3x3 images
+for i in range(0, 9):
+    plt.subplot(330 + 1 + i)
+    plt.imshow(X_train[i], cmap=plt.get_cmap('gray'))
+# Show the plot
+plt.show()
+
+# Params
 NUM_ROWS = 28
 NUM_COLS = 28
 NUM_CLASSES = 10
 BATCH_SIZE = 128
 EPOCHS = 10
+
+# Data augmentation
+# https://machinelearningmastery.com/image-augmentation-deep-learning-keras/
+# from keras.preprocessing.image import ImageDataGenerator
+# # reshape to be [samples][pixels][width][height]
+# X_train = X_train.reshape(X_train.shape[0], 1, 28, 28)
+# X_test = X_test.reshape(X_test.shape[0], 1, 28, 28)
+# # convert from int to float
+# X_train = X_train.astype('float32')
+# X_test = X_test.astype('float32')
+# # define data preparation
+# datagen = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True)
+# # fit parameters from data
+# datagen.fit(X_train)
+
 
 # Reshape data
 X_train = X_train.reshape((X_train.shape[0], NUM_ROWS * NUM_COLS))
@@ -40,10 +66,10 @@ data_summary(X_train, y_train, X_test, y_test)
 
 # Build neural network
 model = models.Sequential()
-model.add(Dense(512, activation='relu', input_shape=(NUM_ROWS * NUM_COLS,)))
-model.add(Dropout(0.5))
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.25))
+model.add(Dense(512, activation='relu', input_shape=(NUM_ROWS * NUM_COLS,), kernel_regularizer=regularizers.l2(0.01)))
+# model.add(Dropout(0.5))
+model.add(Dense(256, activation='relu', kernel_regularizer=regularizers.l1(0.0001)))
+# model.add(Dropout(0.25))
 model.add(Dense(10, activation='softmax'))
 
 # Compile model
@@ -57,7 +83,9 @@ model.fit(X_train, y_train,
           epochs=EPOCHS,
           # callbacks=[plot_losses],
           verbose=1,
-          validation_data=(X_test, y_test))
+          validation_data=(X_test, y_test),
+          # callbacks=[EarlyStopping(monitor='val_acc', patience=2)]
+          )
 
 score = model.evaluate(X_test, y_test, verbose=0)
 print('Test loss:', score[0])
@@ -65,3 +93,17 @@ print('Test accuracy:', score[1])
 
 # Summary of neural network
 model.summary()
+
+# model.save("mnist-model.h3reg")
+
+# Test model for one image
+img = X_test[122]
+test_img = img.reshape((1,784))
+img_class = model.predict_classes(test_img)
+prediction = img_class[0]
+classname = img_class[0]
+print("Class: ",classname)
+img = img.reshape((28,28))
+plt.imshow(img)
+plt.title(classname)
+plt.show()
